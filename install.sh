53c88325ccf46ca2388ea771ce6820bc36e13110
Daniel Nehrig <daniel.nehrig@redteclab.com>
2018-05-04 11:27:31 +0200

    planning

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Installing Dependencies
+### Brew Install Validation
+### Brew Dependencies
+
+### NodeJS NPM Dependencies
+
+### Ruby GEM Dependencies
+
+### Pyhthon PIP Dependencies
+
+# Downloading Private Files if Permission granted
+### SSH KEYS
+
+### Fonts
