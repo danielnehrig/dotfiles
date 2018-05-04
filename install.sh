439bc1b076bfffc078c005ff1399c9ccc186f357
Daniel Nehrig <daniel.nehrig@redteclab.com>
2018-05-04 14:45:38 +0200

    linking files added config for private files dl from remote server

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 ### Install Script by Daniel Nehrig inet-pwnZ
 ### @inetpwnZ // daniel.nehrig@dnehrig.com
+repo="$(pwd)"
 
 # Installing Dependencies
 ### Brew Install Validation
+echo "Enter Permission Credentials"
 if ! brew_loc="$(type -p "brew")" || [[ -z $brew_loc ]]; then
   echo "Installing Brew"
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -16,6 +18,7 @@ fi
 brew_depend="vim mpv mplayer unrar tmux shairport-sync w3m zsh youtube-dl wget wine"
 brew_dev_depend="node ruby python mongodb gdb maven mysql go docker docker-compose docker-machine ctags cmake perl lua"
 if ! brew_loc="$(type -p "brew")" || [[ ! -z $brew_loc ]]; then
+  clear
   echo "Install System Utilitys"
   echo $brew_depend
   brew install $brew_depend
@@ -29,12 +32,14 @@ fi
 ### NodeJS NPM Dependencies
 node_depend_global="webpack nodemon"
 if ! node_loc="$(type -p "npm")" || [[ ! -z $node_loc ]]; then
+  clear
   npm install $node_depend_global --global
 fi
 
 ### Ruby GEM Dependencies
 gem_depend="mailcatcher sass"
 if ! gem_loc="$(type -p "gem")" || [[ ! -z $gem_loc ]]; then
+  clear
   echo "Installing Gem Dependencies"
   echo $gem_depend
   gem install $gem_depend
@@ -43,6 +48,7 @@ fi
 ### Python PIP Dependencies
 pip_depend="pylint setuptools unicorn wheel wrapt youtube-dl Pygments powerline-status mercurial pip isort"
 if ! python_loc="$(type -p "python")" || [[ ! -z $python_loc ]]; then
+  clear
   echo "Installing Pip Dependencies"
   echo $pip_depend
   pip install $pip_depend
@@ -51,6 +57,8 @@ else
 fi
 
 # Make ZSH default shell
+sudo echo "/usr/local/bin/zsh" >> /etc/shells
+chsh -s /usr/local/bin/zsh $(whoami)
 
 # Downloading Private Files if Permission granted
 ### SSH KEYS
@@ -58,3 +66,5 @@ fi
 ### Fonts
 
 # Linking Files
+ln -s $pwd/.vim/vimrc ~/.vimrc
+ln -s $pwd/.zsh/zshrc ~/.zshrc
