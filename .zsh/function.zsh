31a44fdfee592d8fabd1415aad66f929416cd860
Daniel Nehrig <daniel.nehrig@redteclab.com>
2018-05-11 13:52:40 +0200

    changes on how ot locate the shop repo

--- .zsh/function.zsh
+++ .zsh/function.zsh
@@ -1,35 +1,60 @@
 # Work related Functions
+#
+function shopCheckout() {
+  isShopRepo=`git remote -v | grep shop-apotheke`
+  if [ "$?" -ne "0" ]; then
+    echo "Not In Shop Repo"
+    exit 1
+  fi
+  if [ ! -z $1 ];
+  then
+    if [[ $1 =~ '[0-9]{4}' ]]
+    then
+      git checkout bugfix/CSEAV-$1
+    else
+      echo 'No Valid Ticket Number specified'
+    fi
+  else
+    echo 'Needs 1 Arguments'
+  fi
+
+}
 
 function gitcommitcheck() {
-    currentbranch=$(git branch | grep \* | cut -d ' ' -f2 | cut -d '/' -f2)
-    if [[ $currentbranch =~ 'CSEAV-[0-9]{4}' ]]
+  isShopRepo=`git remote -v | grep shop-apotheke`
+  if [ "$?" -ne "0" ]; then
+    echo "Not In Shop Repo"
+    exit 1
+  fi
+  currentbranch=$(git branch | grep \* | cut -d ' ' -f2 | cut -d '/' -f2)
+  if [[ $currentbranch =~ 'CSEAV-[0-9]{4}' ]]
+  then
+    if [[ ! -z $1 ]]
     then
-        if [[ ! -z $1 ]]
-        then
-            commitname=$(echo $currentbranch-$1)
-            git commit -m "$currentbranch: $1"
-        else
-            echo 'No Changes specified'
-        fi
+      commitname=$(echo $currentbranch-$1)
+      git commit -m "$currentbranch: $1"
     else
-        echo 'Not IN Shop Repo'
+      echo 'No Changes specified'
     fi
+  else
+    echo 'Branch has wrong Formating'
+  fi
 }
 
 function gitcommitcheckpupf() {
-    currentbranch=$(git branch | grep \* | cut -d ' ' -f2 | cut -d '/' -f2)
-    if [[ $currentbranch =~ 'CSEAV-[0-9]{4}' ]]
+  currentbranch=$(git branch | grep \* | cut -d ' ' -f2 | cut -d '/' -f2)
+  if [[ $currentbranch =~ 'CSEAV-[0-9]{4}' ]]
+  then
+    if [[ ! -z $1 ]]
     then
-        if [[ ! -z $1 ]]
-        then
-            commitname=$(echo "($currentbranch): $1")
-            git commit -m "fix($currentbranch): $1"
-        else
-            echo 'No Changes specified'
-        fi
+      commitname=$(echo "($currentbranch): $1")
+      git commit -m "fix($currentbranch): $1"
     else
-        echo 'Not IN Pupf Repo'
+      echo 'No Changes specified'
     fi
+  else
+    echo 'Not IN Pupf Repo'
+  fi
 }
 
 function nodeClear() {
