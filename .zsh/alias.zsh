36cea3f542c100f1d9f643a03fb6a98452ab8b15
Daniel Nehrig <daniel.nehrig@redteclab.com>
2018-05-14 10:08:57 +0200

    moved exported work envs into aliases and added hosts

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -3,8 +3,15 @@ alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall 
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
 alias iLock='pmset displaysleepnow'
 alias gatling='vagrant gatling-rsync-auto'
+export SHOP_ENV=~/code/work/vagrantboxes
+export SHOP_CODE=~/code/work/shop-apotheke
+export NODE_SYNC=~/code/work/shopapotheke-sync-new
+export SHOP_SERVICE=~/code/work/frontend-services
+export CODE_FOLDER=~/code/
+
 alias grname='git config --get remote.origin.url'
 alias git-com='gitcommitcheck'
 alias git-pupf='gitcommitcheckpupf'
 alias updateAll='brew update'
 alias gcosh='shopCheckout'
+alias hosts=/etc/hosts
