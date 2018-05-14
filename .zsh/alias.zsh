e2b0585fb0ed912f5cd33d7b576000de35f82ceb
Daniel Nehrig <daniel.nehrig@redteclab.com>
2018-05-14 10:10:15 +0200

    fixes in aliases

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,17 +1,17 @@
-alias ca='clear && archey'
-alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
-alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
-alias iLock='pmset displaysleepnow'
-alias gatling='vagrant gatling-rsync-auto'
 export SHOP_ENV=~/code/work/vagrantboxes
 export SHOP_CODE=~/code/work/shop-apotheke
 export NODE_SYNC=~/code/work/shopapotheke-sync-new
 export SHOP_SERVICE=~/code/work/frontend-services
 export CODE_FOLDER=~/code/
+export HOSTS=/etc/hosts
 
+alias ca='clear && archey'
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+alias iLock='pmset displaysleepnow'
+alias gatling='vagrant gatling-rsync-auto'
 alias grname='git config --get remote.origin.url'
 alias git-com='gitcommitcheck'
 alias git-pupf='gitcommitcheckpupf'
 alias updateAll='brew update'
 alias gcosh='shopCheckout'
-alias hosts=/etc/hosts
