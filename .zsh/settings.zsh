c66ab34ca1f674e20f92a01e20c0b7902bae0f0e
Daniel Nehrig <daniel.nehrig@redteclab.com>
2019-10-28 13:18:42 +0100

    updated fzf

--- .zsh/settings.zsh
+++ .zsh/settings.zsh
@@ -24,18 +24,3 @@ POWERLEVEL9K_TIME_FORMAT="%D{%H:%M \uE868  %d.%m.%y}"
 
 POWERLEVEL9K_STATUS_VERBOSE=false
 POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
-
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
-fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
-export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git --exclude npm-packages-offline-cache --exclude node_modules'
