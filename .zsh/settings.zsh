184c622bef2f2742a040e038e565d8ea0ad8d650
Daniel Nehrig <daniel.nehrig@redteclab.com>
2020-02-18 10:23:15 +0100

    changed glyph type to nerd-fonts

--- .zsh/settings.zsh
+++ .zsh/settings.zsh
@@ -1,4 +1,5 @@
-POWERLEVEL9K_MODE='awesome-patched'
+# POWERLEVEL9K_MODE='awesome-patched'
+POWERLEVEL9K_MODE='nerdfont-complete'
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
 DISABLE_AUTO_TITLE='true'
