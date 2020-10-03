c8a2b953d7f684a1b728c02b29d033e8b1721f4a
danielnehrig <daniel.nehrig@dnehrig.com>
2020-10-03 19:42:21 +0200

    check if darwin or linux

--- .zsh/export.zsh
+++ .zsh/export.zsh
@@ -2,15 +2,19 @@
 export ZSH="$HOME/.dotfiles-darwin/oh-my-zsh"
 
 # Adding Path Data Linux
+if [[ `uname` == "Linux"  ]]; then
 PATH+=":$HOME/.local/lib/python3.8/site-packages"
 PATH+=":$HOME/.local/bin"
 PATH+=":$HOME/.cargo/bin"
+fi
 
 # LDFLAGS Mac
-# export LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib"
 
 # User exports
-# export PATH="/usr/local/sbin:/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include:/usr/local/sbin:/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr:/usr/local/bin:/usr/local/Cellar:/usr/bin:/bin:/usr/sbin:/sbin:/usr/include/libxml2:/Users/$(id -un)/Library/Python/2.7/bin:/usr/local/opt/llvm/bin:/usr/local/include:/$(id -un)/dnehrig/Library/Python/3.7/bin:/usr/local/share/dotnet:/Library/TeX/texbin"
+if [[ `uname` == "Darwin"  ]]; then
+export LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib"
+export PATH="/usr/local/sbin:/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include:/usr/local/sbin:/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr:/usr/local/bin:/usr/local/Cellar:/usr/bin:/bin:/usr/sbin:/sbin:/usr/include/libxml2:/Users/$(id -un)/Library/Python/2.7/bin:/usr/local/opt/llvm/bin:/usr/local/include:/$(id -un)/dnehrig/Library/Python/3.7/bin:/usr/local/share/dotnet:/Library/TeX/texbin"
+fi
 export EDITOR='vim'
 export DEFAULT_USER="$USER"
 export TERM=xterm-256color
