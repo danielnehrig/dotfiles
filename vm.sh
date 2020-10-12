873d0ba6de1c6c294a084506f72e3a2ceb612acd
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2020-10-12 11:07:05 +0200

    added vm script to check for running vms

---
+++ vm.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+#
+# vm.sh
+# Copyright (C) 2020 dashie <dashie@h3k9fb1>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+amount=$(virsh -c qemu:///system list --all | grep running | wc -l)
+
+if [[ $amount == '0' ]]; then
+  echo "󱐝"
+else
+  echo "󰆧 $amount VM"
+fi
