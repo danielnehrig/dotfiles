a59edda4d4acc25935b3d03621a13ba7dc97b4fc
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2020-10-13 14:12:03 +0200

    speed formating fix

--- themes/polybar/arrow/scripts/speed.sh
+++ themes/polybar/arrow/scripts/speed.sh
@@ -8,12 +8,13 @@
 
 ### crontab job (pacman -S cronie)
 ### crontab -e
-# 0 * * * * speedtest-cli > /home/dashie/.config/polybar/scripts/speedlog
+### every hour and 10min start speedtest
+# 10 * * * * speedtest-cli > /home/dashie/.config/polybar/scripts/speedlog
 
 speed=$(cat ~/.config/polybar/scripts/speedlog)
 download=$(printf "$speed" | grep Download)
 upload=$(printf "$speed" | grep Upload)
-downloadFormated=$(printf "$speed" | grep Download | sed -n s/Download://gp | sed -n s/^' '//gp | sed -n 's|Mbit/s||gp')
-uploadFormated=$(printf "$speed" | grep Upload | sed -n s/Upload://gp | sed -n s/^' '//gp | sed -n 's|Mbit/s||gp')
+downloadFormated=$(printf "$speed" | grep Download | sed -n 's|.*Download:||gp' | sed -n 's| ||gp' | sed -n 's|Mbit/s||gp')
+uploadFormated=$(printf "$speed" | grep Upload | sed -n 's|.*Upload:||gp' | sed -n 's| ||gp' | sed -n 's|Mbit/s||gp')
 
 echo "%{T3}%{F$1}%{F-} %{T-}$downloadFormated  %{T3}%{F$1}%{F-} %{T-}$uploadFormated"
