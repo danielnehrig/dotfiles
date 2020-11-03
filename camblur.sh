42c7aa52369537dafd08ad38bc2eabaf18ef2e9d
Daniel Nehrig <daniel.nehrig@redteclab.com>
2020-11-03 07:27:34 +0100

    blur

---
+++ camblur.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# cam.sh
+# Copyright (C) 2020 dashie <dashie@h3k9fb1>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+pkill -f /dev/video || mpv --no-osc --geometry=-0-0 --autofit=30% av://v4l2:/dev/video2 --profile=low-latency --untimed
