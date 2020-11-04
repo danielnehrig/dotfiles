41e36eafa157d93406636310e54f99a83bd35bc5
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2020-11-04 01:42:25 +0100

    removed unused funcs zsh

--- .zsh/function.zsh
+++ .zsh/function.zsh
@@ -14,49 +14,6 @@ function printColors() {
   for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
 }
 
-function shopUpdate() {
-  if [ ! -z $SHOP_ENV ];
-  then
-    echo "Update Vagrent ENV"
-    cd $SHOP_ENV
-    ./update.sh
-    echo "Update Shop-Repo"
-    cd $SHOP_CODE
-    git pull
-    echo "Update NodeSync"
-    cd $NODE_SYNC
-    git pull
-    echo "Update Shop Services"
-    cd $SHOP_SERVICE
-    cd pupf-client
-    git pull
-  else
-    echo "env not set"
-  fi
-}
-
-function shopCheckout() {
-  isShopRepo=$(git remote -v)
-  if ! [[ $isShopRepo =~ '(shop-apotheke|shopeav)' ]]; then
-      echo "Not In Shop Repo"
-      return
-  fi
-  if [ ! -z $1 ];
-  then
-    if [[ $1 =~ '[0-9]{4}' ]]
-    then
-      git checkout bugfix/CSEAV-$1
-    else
-      echo 'No Valid Ticket Number specified'
-      return
-    fi
-  else
-    echo 'Needs 1 Arguments'
-    return
-  fi
-
-}
-
 function gitcommitcheck() {
   isShopRepo=$(git remote -v)
   if ! [[ $isShopRepo =~ '(shop-apotheke|shopeav)' ]]; then
@@ -117,22 +74,11 @@ function tarPackUnpack() {
 function nodeClear() {
   rm -rf node_modules
   rm package-lock.json
-  node cache verify
-  node install
+  npm cache verify
+  yarn || npm
 }
 
 # Random
-
-function darkTheme() {
-  osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/dnehrig/wallpaper/dark.jpg"'
-  dark-mode on
-}
-
-function lightTheme() {
-  osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/dnehrig/wallpaper/light.jpg"'
-  dark-mode off
-}
-
 function countCode() {
   find . -name '*.$1' | xargs wc -l
 }
