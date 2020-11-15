e810408ec87a1c3678f9b023ca5df238742d7f3b
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2020-11-15 04:44:25 +0100

    screenrecord

--- scripts/screenrecord.sh
+++ scripts/screenrecord.sh
@@ -6,4 +6,4 @@
 # Distributed under terms of the MIT license.
 #
 
-pkill -f ffmpeg && notify-send "ffmpeg stopped" || ffmpeg -y -video_size 2880x1800 -framerate 30 -f x11grab -i :1.0 -f pulse -ac 2 -i 0 ~/screencast/screen1_recording_`date '+%Y-%m-%d_%H-%M-%S'`.mp4 && notify-send "Record screen1_recording_`date '+%Y-%m-%d_%H-%M-%S'`.mp4"
+pkill -f ffmpeg && notify-send "ffmpeg stopped" || ffmpeg -y -video_size 1920x1200 -framerate 30 -f x11grab -i :0.0 -f pulse -ac 2 -i 0 ~/screencast/screen1_recording_`date '+%Y-%m-%d_%H-%M-%S'`.mp4 && notify-send "Record screen1_recording_`date '+%Y-%m-%d_%H-%M-%S'`.mp4"
