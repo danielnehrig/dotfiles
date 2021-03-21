b182b4f55eafe2e790762a2176a720e3d48f5681
Daniel Nehrig <daniel.nehrig@redteclab.com>
2021-03-21 20:51:30 +0100

    add clang vimls and lua

--- .config/nvim/lua/nvim-lspconfig/lua.lua
+++ .config/nvim/lua/nvim-lspconfig/lua.lua
@@ -27,9 +27,9 @@ local custom_attach = function(client)
 	map('n','<space>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
 	map('n','<space>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
 	map('n','<space>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
+	map('i','<C-space>','<cmd>lua vim.lsp.buf.completion()<CR>')
 end
 
---  lsp for html , css and js/ts
 require "lspconfig".tsserver.setup{on_attach=custom_attach}
 require "lspconfig".rls.setup{on_attach=custom_attach}
 require "lspconfig".cssls.setup{on_attach=custom_attach}
@@ -37,4 +37,47 @@ require "lspconfig".html.setup{on_attach=custom_attach}
 require "lspconfig".rust_analyzer.setup{on_attach=custom_attach}
 require "lspconfig".gopls.setup{on_attach=custom_attach}
 require "lspconfig".pyright.setup{on_attach=custom_attach}
+require "lspconfig".dockerls.setup{on_attach=custom_attach}
+require "lspconfig".clangd.setup{on_attach=custom_attach}
+require "lspconfig".vimls.setup{on_attach=custom_attach}
 
+local system_name
+if vim.fn.has("mac") == 1 then
+  system_name = "macOS"
+elseif vim.fn.has("unix") == 1 then
+  system_name = "Linux"
+elseif vim.fn.has('win32') == 1 then
+  system_name = "Windows"
+else
+  print("Unsupported system for sumneko")
+end
+
+-- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
+local sumneko_root_path = '/Users/dnehrig/.dotfiles-darwin/lua-language-server'
+local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
+
+require "lspconfig".sumneko_lua.setup {
+	on_attach=custom_attach,
+  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = vim.split(package.path, ';'),
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        },
+      },
+    },
+  },
+}
