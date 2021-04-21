7c36f887f4dfa9026ba0799886fb7501d51f41b3
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-04-21 21:28:54 +0200

    one

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -6,6 +6,7 @@ remap("n", "<Leader>qc", ":cclose<CR>", {silent = true, noremap = true})
 remap("n", "<Leader>qn", ":cnext<CR>", {silent = true, noremap = true})
 remap("n", "<Leader>qo", ":copen<CR>", {silent = true, noremap = true})
 remap("n", "<Leader>qp", ":cprev<CR>", {silent = true, noremap = true})
+remap("n", "<Leader>qa", ":cc<CR>", {silent = true, noremap = true})
 remap("n", "<Leader>r", ":lua require('nvim-reload').Reload()<CR>", {silent = true, noremap = true})
 
 -- telescope
