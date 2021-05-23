7c67f518084c447da20ecd97fa21ecde14069d33
Daniel Nehrig <daniel.nehrig@shop-apotheke.com>
2021-05-23 15:39:38 +0200

    rust analyzer install

--- install.py
+++ install.py
@@ -403,7 +403,7 @@ def Linux():
 
     # cloning dependencies zsh theme and plugins
     try:
-        Install('sudo pacman -S base-devel nvidia zsh docker docker-compose alacritty network-manager-applet kubectl xclip go rustup clang gcc cmake lightdm lightdm-webkit2-greeter vim tmux i3-gaps xorg networkmanager pulseaudio bat fd ripgrep neofetch python2 pyhton2-pip python python-pip')
+        Install('sudo pacman -S base-devel nvidia zsh docker docker-compose alacritty network-manager-applet kubectl xclip go rustup clang gcc cmake lightdm lightdm-webkit2-greeter vim tmux i3-gaps xorg networkmanager pulseaudio bat fd ripgrep neofetch python2 pyhton2-pip python python-pip rust-analyzer')
         Install('yay -S nodenv-git nodenv-node-build brave-bin python-pynvim ueberzug neovim-nightly-git dunst-git polybar-git rofi-git picom-ibhagwan-git ttf-material-design-icon-webfont ttf-nerd-fonts-hack-complete-git')
 
         # nodenv setup
