7583055e17794c6f84685482e4855cce1a559e47
danielnehrig <daniel.nehrig@dnehrig.com>
2021-05-30 15:17:16 +0200

    refactor opt

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -10,17 +10,12 @@ local function load_options()
 
     -- activate line numbers
     -- TODO: make toggleable for pair programming
-    vim.api.nvim_win_set_option(0, "number", true)
-    vim.api.nvim_win_set_option(0, "relativenumber", true)
+    vim.opt.number = true
+    vim.opt.relativenumber = true
 
     -- Set so that folders are index for find command
-    vim.o.path = vim.o.path .. "**/*"
-    vim.o.wildignore = vim.o.wildignore .. "node_modules,.git"
-
-    -- save view on leave and load on buf enter
-    -- used for manuall fold loading
-    -- autocmd("BufWinLeave", "*.*", "mkview")
-    -- autocmd("BufWinEnter", "*.*", "silent loadview")
+    vim.opt.path = vim.o.path .. "**/*"
+    vim.opt.wildignore = vim.o.wildignore .. "node_modules,.git"
 
     -- neovide should not be transparent
     if not vim.g.neovide then
@@ -34,35 +29,35 @@ local function load_options()
     b.match_words = table.concat({"(:),\\[:\\],{:},<:>,", "<\\@<=\\([^/][^ \t>]*\\)[^>]*\\%(>\\|$\\):<\\@<=/\1>"})
 
     cmd "colorscheme gruvbox" -- :)
-    M.opt("o", "hidden", true) -- buffer hidden
-    M.opt("o", "ignorecase", true) -- case sens ignore search
-    M.opt("o", "splitbelow", true) -- split behavior
-    M.opt("o", "splitright", true) -- split behavior
-    M.opt("o", "termguicolors", true) -- color stuff
-    M.opt("o", "t_Co", "256") -- color stuff
-    M.opt("o", "t_ut", "")
-    M.opt("o", "background", "dark") -- dark
-    M.opt("o", "numberwidth", 2) -- width on numbeer row
+    vim.opt.hidden = true -- buffer hidden
+    vim.opt.ignorecase = true -- case sens ignore search
+    vim.opt.splitbelow = true -- split behavior
+    vim.opt.splitright = true -- split behavior
+    vim.go.termguicolors = true
+    vim.go.t_Co = "256"
+    vim.go.t_ut = ""
+    vim.opt.background = "dark" -- dark
+    vim.opt.numberwidth = 2 -- width on numbeer row
 
-    M.opt("o", "mouse", "a") -- mouse on don't use mouse lol
+    vim.opt.mouse = "a" -- mouse on don't use mouse lol
 
-    M.opt("w", "signcolumn", "auto") -- 2 sign column
-    M.opt("o", "cmdheight", 1)
-    M.opt("o", "guifont", "FiraCode Nerd Font Mono:h12")
+    vim.opt.signcolumn = "auto" -- 2 sign column
+    vim.opt.cmdheight = 1
+    vim.opt.guifont = "FiraCode Nerd Font Mono:h12"
 
     if vim.g.neovide then
         cmd('let g:neovide_cursor_vfx_mode = "pixiedust"')
     end
 
-    M.opt("o", "updatetime", 300) -- update interval for gitsigns
-    M.opt("o", "inccommand", "nosplit")
-    vim.cmd("set timeoutlen=500")
-    M.opt("o", "clipboard", "unnamedplus") -- clipboard yank
+    vim.opt.updatetime = 300 -- update interval for gitsigns
+    vim.opt.inccommand = "nosplit"
+    vim.opt.timeoutlen = 500
+    vim.opt.clipboard = "unnamedplus" -- clipboard yank
 
     -- for indenline
     -- indentation settings
-    M.opt("b", "expandtab", true)
-    M.opt("b", "shiftwidth", 2)
+    vim.opt.expandtab = true
+    vim.opt.shiftwidth = 2
 end
 
 load_options()
