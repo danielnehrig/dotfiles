342416cd5806116e42eee61e957a5fe86559773a
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-06-18 21:21:00 +0200

    terminall stuff

--- .config/nvim/lua/core/autocmd.lua
+++ .config/nvim/lua/core/autocmd.lua
@@ -4,6 +4,11 @@ local definitions = {
     ft = {
         {"FileType", "dashboard", "set showtabline=0"}, -- disable tabline in dashboard
         {"BufNewFile,BufRead", "*", "set showtabline=2"}, -- renable it
+        {"BufNewFile,BufRead", "*", "set number"}, -- set number
+        {"BufNewFile,BufRead", "*", "set relativenumber"}, -- set relativenumber
+        {"TermOpen", "*", "set number!"},
+        {"TermOpen", "*", "set relativenumber!"},
+        {"TermOpen", "*", "set showtabline=2"}, -- renable it
         {"BufNewFile,BufRead", "*.toml", " setf toml"} -- set toml filetype
     }
 }
