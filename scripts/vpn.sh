693b28adae94d9d8401542919233bae3cf8a2686
Daniel Nehrig <daniel.nehrig@shop-apotheke.com>
2021-06-23 13:33:56 +0200

    vpn

--- scripts/vpn.sh
+++ scripts/vpn.sh
@@ -1,7 +1,22 @@
+#! /bin/sh
 cd $HOME/vpn
 
 VPN=$(ls *.ovpn | rofi -dmenu -p "VPN: ")
+USER=dNehrig
+MASTERPW=$(printf '' | rofi -dmenu -p "BW PW" -password -lines 0) || exit $?
+PW=$(echo $MASTERPW | bw get password OWA)
+TOKEN=$(printf '' | rofi -dmenu -p "TOTP: " -password -lines 0) || exit $?
+PWTOKEN=$PW$TOKEN
+SUDA=$(printf '' | rofi -dmenu -p "SUDO: " -password -lines 0) || exit $?
+
+rm $HOME/vpn/login.conf
+echo $USER >> $HOME/vpn/login.conf
+echo $PWTOKEN >> $HOME/vpn/login.conf
 
 [ -z "$VPN" ] && exit
+[ -z "$USER" ] && exit
+[ -z "$PWTOKEN" ] && exit
+[ -z "$SUDA" ] && exit
 
-pkill -f openvpn && alacritty -e /bin/bash -c "sudo openvpn $HOME/vpn/$VPN"
+pkill -f openvpn
+echo $SUDA | sudo -S openvpn --config /home/dnehrig/vpn/$VPN --auth-user-pass /home/dnehrig/vpn/login.conf
