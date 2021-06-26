e8c0364c6b899a6fc32bdf0522ee6cb01aa79b16
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-06-26 20:23:17 +0200

    remove old arrow polybar

--- themes/polybar/arrow-old/scripts/date.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env bash
-
-date=$(date '+%e/%m/%y %k:%M')
-
-echo "%{T2}$date%{T-}"
-
-
--- themes/polybar/arrow-old/scripts/toogle.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-[[ "$(playerctl -p spotify status 2>/dev/null)" == "Playing" ]] && echo "" || echo ""
