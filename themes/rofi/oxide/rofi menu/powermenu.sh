2dd7db266474ff9b5f635484a00bf66231c70450
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-07-04 21:36:09 +0200

    oxide

--- themes/rofi/oxide/rofi menu/powermenu.sh
+++ themes/rofi/oxide/rofi menu/powermenu.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 prais="~/.config/rofi/rofi menu/powerlayout.rasi"
-rofi_command="rofi -dpi 150 -theme"
+rofi_command="rofi -dpi 150"
 uptime=$(uptime -p | sed -e 's/up //g')
 
 # Options
-shutdown="󰿅"
-reboot="󰜉"
-lock="󰌾"
+shutdown="shutdown"
+reboot="reboot"
+lock="lock"
 
 # Variable passed to rofi
 options="$shutdown\n$reboot\n$lock"
 
-chosen="$(echo -e "$options" | $rofi_command "$prais" -p "UP - $uptime" -dmenu -selected-row 2)"
+chosen="$(echo -e "$options" | $rofi_command -p "UP - $uptime" -dmenu -selected-row 2)"
 case $chosen in
     $shutdown)
         systemctl poweroff
