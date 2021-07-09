297da85ffca9397129e27ff59a8554ceacc61a51
Daniel Nehrig <daniel.nehrig@shop-apotheke.com>
2021-07-09 13:11:17 +0200

    remove short path

--- .config/nvim/lua/plugins/telescope/init.lua
+++ .config/nvim/lua/plugins/telescope/init.lua
@@ -37,7 +37,6 @@ function load()
             file_sorter = require "telescope.sorters".get_generic_sorter,
             file_ignore_patterns = {},
             generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
-            shorten_path = true,
             winblend = 0,
             border = {},
             borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
