f39c9fbea6974b96ffc628244d9d779d7258de7d
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-07-11 15:02:42 +0200

    add pc

--- scripts/screenrecord.sh
+++ scripts/screenrecord.sh
@@ -7,11 +7,15 @@
 #
 
 pc="dnehrig"
+priv="dashie"
 macbook="lain"
 filePath=~/screencast/screen1_recording_`date '+%Y-%m-%d_%H-%M-%S'`.mp4
 SERVICE="ffmpeg"
 
 if [[ "$USER" == "$pc" ]]
+then
+  pkill -f ffmpeg && notify-send "ffmpeg stopped" || ffmpeg -y -video_size 1920x1080 -framerate 30 -f x11grab -i :0.0 -f pulse -ac 2 -i 0 ~/screencast/screen1_recording_`date '+%Y-%m-%d_%H-%M-%S'`.mp4 && notify-send "Record screen1_recording_`date '+%Y-%m-%d_%H-%M-%S'`.mp4"
+elif [[ "$USER" == "$priv" ]]
 then
   pkill -f ffmpeg && notify-send "ffmpeg stopped" || ffmpeg -y -video_size 1920x1080 -framerate 30 -f x11grab -i :0.0 -f pulse -ac 2 -i 0 ~/screencast/screen1_recording_`date '+%Y-%m-%d_%H-%M-%S'`.mp4 && notify-send "Record screen1_recording_`date '+%Y-%m-%d_%H-%M-%S'`.mp4"
 elif [[ "$USER" == "$macbook" ]]
