e7b40278a5369fa6f20df89d54fe3ca8e95730f2
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-07-15 21:59:14 +0200

    bash

--- .config/nvim/lua/plugins/lspconfig/bash.lua
+++ .config/nvim/lua/plugins/lspconfig/bash.lua
@@ -2,4 +2,10 @@ local lsp = require("plugins.lspconfig")
 local capabilities = require("plugins.lspconfig").capabilities
 local lspconfig = require("lspconfig")
 
-lspconfig.bashls.setup{}
+lspconfig.bashls.setup {
+    capabilities = capabilities,
+    flags = {debounce_text_changes = 500},
+    on_attach = function(client, bufnr)
+        lsp:on_attach(client, bufnr)
+    end
+}
