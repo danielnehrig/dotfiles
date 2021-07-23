58044a19aecd2ed746c79df198f5a05fc9c5c895
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-07-23 19:18:10 +0200

    spell check

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,12 +1,10 @@
 local cmd = vim.cmd
 local g, b, opt, go = vim.g, vim.b, vim.opt, vim.go
-local nvim_command = vim.api.nvim_command
--- local autocmd = require "utils".autocmd
 
 local function load_options()
     opt.shadafile = "NONE"
     -- activate line numbers
-    -- TODO: make toggleable for pair programming
+    -- TODO: make toggle for pair programming
     opt.number = true -- enable numbers
     opt.relativenumber = true -- enable numbers to be relative
 
@@ -22,6 +20,7 @@ local function load_options()
 
     -- Tag Jump
     b.match_words = table.concat({"(:),\\[:\\],{:},<:>,", "<\\@<=\\([^/][^ \t>]*\\)[^>]*\\%(>\\|$\\):<\\@<=/\1>"})
+    opt.matchpairs:append("<:>")
 
     opt.hidden = true -- buffer hidden
     opt.ignorecase = true -- case sens ignore search
@@ -31,14 +30,16 @@ local function load_options()
     go.t_Co = "256" -- colors tmux setting
     go.t_ut = "" -- colors tmux setting
     -- opt.background = "dark" -- dark
-    opt.numberwidth = 2 -- width on numbeer row
+    opt.numberwidth = 2 -- width on number row
 
-    opt.mouse = "a" -- mouse on don't use mouse lol
+    opt.mouse = "a" -- mouse on don't use mouse
 
     opt.signcolumn = "auto" -- 2 sign column
     opt.cmdheight = 1 -- ex cmd height
     opt.guifont = "Hack Nerd Font Mono:h12" -- set font
-    opt.showcmd = false -- disable showcmd
+    opt.showcmd = false -- disable showcmd keys bottom right
+    opt.showmode = false -- modes
+    opt.autoread = true -- reload files changed other edit
 
     if g.neovide then
         cmd('let g:neovide_cursor_vfx_mode = "pixiedust"')
@@ -48,11 +49,21 @@ local function load_options()
     opt.inccommand = "nosplit"
     opt.timeoutlen = 500
     opt.clipboard = "unnamedplus" -- clipboard yank
+    opt.wildmenu = true
+    opt.fileformats = "unix,dos,mac"
 
-    -- for indenline
+    -- fast
+    opt.ttyfast = true
+    opt.lazyredraw = true
+
+    -- for indentline
     -- indentation settings
     opt.expandtab = true
     opt.shiftwidth = 2
+
+    -- spell
+    opt.spelllang = "en"
+    opt.spell = true
 end
 
 load_options()
