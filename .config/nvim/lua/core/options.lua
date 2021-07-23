3985bd2dd258700f3c2b8b901c9abf0ac0e81e8c
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-07-23 19:02:21 +0200

    comments on options

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -7,39 +7,38 @@ local function load_options()
     opt.shadafile = "NONE"
     -- activate line numbers
     -- TODO: make toggleable for pair programming
-    opt.number = true
-    opt.relativenumber = true
+    opt.number = true -- enable numbers
+    opt.relativenumber = true -- enable numbers to be relative
 
     -- completion menu settings
-    opt.completeopt = "menuone,noselect"
-    opt.omnifunc = "v:lua.vim.lsp.omnifunc"
+    opt.completeopt = "menuone,noselect" -- completion behaviour
+    opt.omnifunc = "v:lua.vim.lsp.omnifunc" -- completion omnifunc
 
     -- Set so that folders are index for find command
-    opt.path:append("**/*")
-    opt.wildignore:append("node_modules,.git")
+    opt.path:append({"**/*"})
+    opt.wildignore:append({"node_modules", ".git", "dist", ".next"})
 
-    g.mapleader = " "
-    g.beacon_enable = 1
+    g.mapleader = " " -- space leader
 
     -- Tag Jump
     b.match_words = table.concat({"(:),\\[:\\],{:},<:>,", "<\\@<=\\([^/][^ \t>]*\\)[^>]*\\%(>\\|$\\):<\\@<=/\1>"})
 
-    nvim_command("autocmd FileType NvimTree,lspsagafinder,dashboard,vista let b:cursorword = 0")
     opt.hidden = true -- buffer hidden
     opt.ignorecase = true -- case sens ignore search
     opt.splitbelow = true -- split behavior
     opt.splitright = true -- split behavior
-    go.termguicolors = true
-    go.t_Co = "256"
-    go.t_ut = ""
+    go.termguicolors = true -- colors tmux settings
+    go.t_Co = "256" -- colors tmux setting
+    go.t_ut = "" -- colors tmux setting
     -- opt.background = "dark" -- dark
     opt.numberwidth = 2 -- width on numbeer row
 
     opt.mouse = "a" -- mouse on don't use mouse lol
 
     opt.signcolumn = "auto" -- 2 sign column
-    opt.cmdheight = 1
-    opt.guifont = "Hack Nerd Font Mono:h12"
+    opt.cmdheight = 1 -- ex cmd height
+    opt.guifont = "Hack Nerd Font Mono:h12" -- set font
+    opt.showcmd = false -- disable showcmd
 
     if g.neovide then
         cmd('let g:neovide_cursor_vfx_mode = "pixiedust"')
