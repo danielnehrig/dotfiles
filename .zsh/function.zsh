f7d53f5af8a8007b8f75e10280fc9029d78883bb
danielnehrig <daniel.nehrig@dnehrig.com>
2022-02-21 09:51:02 +0100

    onefetch
    Signed-off-by: danielnehrig <daniel.nehrig@dnehrig.com>

--- .zsh/function.zsh
+++ .zsh/function.zsh
@@ -157,3 +157,17 @@ function swagger_preview() {
       echo "Converting to json failed!"
     fi
 }
+
+LAST_REPO=""
+
+cd() {
+  builtin cd "$@"
+  git rev-parse 2>/dev/null
+
+  if [ $? -eq 0 ]; then
+    if [ "$LAST_REPO" != $(basename $(git rev-parse --show-toplevel)) ]; then
+      onefetch
+      LAST_REPO=$(basename $(git rev-parse --show-toplevel))
+    fi
+  fi
+}
